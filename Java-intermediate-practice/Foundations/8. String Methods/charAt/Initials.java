01ccd62e23858a2a24bac57dcb400b8bb5c50e55
Paul Veillard <36556329+veilair@users.noreply.github.com>
2022-03-16 12:42:49 -0400

    Add files via upload

---
+++ Java-intermediate-practice/Foundations/8. String Methods/charAt/Initials.java
@@ -0,0 +1,15 @@
+public class Initials {
+  
+	public static void main(String[] args) {
+    
+    // Add a first name and a last name:
+    String firstName = "Kelli";  
+    String lastName = "Davis";
+    
+    // What are the initials?
+    System.out.println(firstName.charAt(0));
+    System.out.println(lastName.charAt(0));
+    
+  }
+  
+}
